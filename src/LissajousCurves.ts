ea07c9a80d75d6fee218b0776f4b9898136a6da6
timmoth <t.jones@timmoth.com>
2023-01-05 20:54:03 +0000

    Initial commit

---
+++ src/LissajousCurves.ts
@@ -0,0 +1,74 @@
+export class LissajousCurves {
+  width: number;
+  height: number;
+  phaseDifference: number;
+  points: [number, number][];
+  phases: string[];
+
+  frequencyRatio: number;
+  deltaPhaseDifference: number;
+
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+
+    this.points = [
+      [1, 1],
+      [1, 2],
+      [1, 3],
+      [2, 3],
+      [3, 4],
+      [3, 5],
+      [4, 5],
+      [5, 6],
+    ];
+    this.phases = ["0", "π/2", "3π/4", "π"];
+
+    this.phaseDifference = 0.1;
+    this.deltaPhaseDifference = 0.01;
+    this.frequencyRatio = 0;
+  }
+
+  draw(ctx: CanvasRenderingContext2D): void {
+    if (this.phaseDifference >= Math.PI) {
+      this.frequencyRatio = (this.frequencyRatio + 1) % this.points.length;
+      this.deltaPhaseDifference *= -1;
+    } else if (this.phaseDifference <= 0) {
+      this.deltaPhaseDifference *= -1;
+    }
+
+    this.phaseDifference += this.deltaPhaseDifference;
+
+    var a = this.points[this.frequencyRatio][0];
+    var b = this.points[this.frequencyRatio][1];
+
+    var A = this.width / 2 - 50;
+    var B = this.height / 2 - 50;
+
+    var t = 0;
+    var x = A * Math.sin(a * t + this.phaseDifference);
+    var y = B * Math.sin(b * t);
+    ctx.fillStyle = "black";
+    ctx.lineWidth = 3;
+    ctx.beginPath();
+    ctx.moveTo(this.width / 2 + x, this.height / 2 + y);
+    for (var i = 0; i < 1000; i++) {
+      t += 0.01;
+      x = A * Math.sin(a * t + this.phaseDifference);
+      y = B * Math.sin(b * t);
+
+      ctx.lineTo(this.width / 2 + x, this.height / 2 + y);
+    }
+    ctx.stroke();
+    ctx.closePath();
+    ctx.font = "25px pixel";
+    ctx.fillStyle = "black";
+    ctx.textBaseline = "top";
+    var text = `Freq ratio: ${a}:${b}, Phase: ${
+      this.phases[
+        Math.round((this.phaseDifference / Math.PI) * (this.phases.length - 1))
+      ]
+    }`;
+    ctx.fillText(text, 10, this.height - 30);
+  }
+}
---
+++ src/LissajousCurvesApp.ts
@@ -0,0 +1,48 @@
+import { LissajousCurves } from "./LissajousCurves";
+
+export default class LissajousCurvesApp {
+  ctx: CanvasRenderingContext2D;
+  canvas: HTMLCanvasElement;
+  lissajousCurves: LissajousCurves;
+  lastTimeStamp: number;
+
+  constructor() {
+    this.canvas = document.getElementById(
+      "LissajousCurves-canvas"
+    ) as HTMLCanvasElement;
+
+    if (this.canvas == null) {
+      throw new Error("Could not find canvas.");
+    }
+
+    this.ctx = this.canvas.getContext("2d") as CanvasRenderingContext2D;
+    this.ctx.imageSmoothingEnabled = false;
+    this.canvas.width = 500;
+    this.canvas.height = 500;
+
+    this.lissajousCurves = new LissajousCurves(
+      this.canvas.width,
+      this.canvas.height
+    );
+
+    this.lastTimeStamp = 0;
+  }
+
+  Start() {
+    this.animate(0);
+  }
+
+  animate(timestamp: number) {
+    if (timestamp - this.lastTimeStamp >= 10) {
+      this.lastTimeStamp = timestamp;
+      this.draw();
+    }
+    requestAnimationFrame((n) => this.animate(n));
+  }
+
+  draw() {
+    this.ctx.fillStyle = "#94d300";
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.lissajousCurves.draw(this.ctx);
+  }
+}
